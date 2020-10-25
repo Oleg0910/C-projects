4e0b0746d340955480d0c42f615528693a3b3975
Oleg0910 <09102004oleg@gmail.com>
2020-10-25 19:06:27 +0200

    Create 4.cpp
    Create a new file

---
+++ 4.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+void func(float &y,float &z)
+{
+    float sum, riz;
+    sum = y + z;
+    riz = y - z;
+    y = sum;
+    z = riz;
+
+}
+
+int main()
+{
+    float a = 2.1;
+    float b = 1.1;
+    func(a,b);
+    cout << a << " " << b;
+}
+
+
